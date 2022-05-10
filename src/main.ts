adfb660f239bd7293676b12e0aaf3602320b44bb
wudidesheng <206855707@qq.com>
2022-05-11 00:59:20 +0800

    fix:class-vaildator

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 全局管道
+  app.useGlobalPipes(new ValidationPipe());
   Logger.log('server run in:http://127.0.0.1:3000/');
+
   await app.listen(3000);
 }
 bootstrap();
--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,5 +1,8 @@
+import { IsNotEmpty } from 'class-validator';
 export class CreateTaskDto {
+  @IsNotEmpty()
   id: string;
+  @IsNotEmpty()
   name: string;
   description: string;
   status: 'doing' | 'done';
